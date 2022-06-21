69e9f9edf7aa82c317e31cedeed2c85e1c4ad190
TuanPhamHP <phamanhtuan290896@gmail.com>
2022-06-21 15:27:41 +0700

    update: add img

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const timeSet = 10000;
+	const listLh0 = [...document.querySelectorAll('.lh0-radio')];
 	const listLh1 = [...document.querySelectorAll('.lh1-radio')];
 	const listLh2 = [...document.querySelectorAll('.lh2-radio')];
 	const listLh3 = [...document.querySelectorAll('.lh3-radio')];
+	const previewLh0 = document.querySelector('#lh0-preview-image');
 	const previewLh1 = document.querySelector('#lh1-preview-image');
 	const previewLh2 = document.querySelector('#lh2-preview-image');
 	const previewLh3 = document.querySelector('#lh3-preview-image');
+	listLh0.forEach(o => {
+		if (o.checked) {
+			previewLh0.setAttribute('src', `./assets/images/${o.getAttribute('preview-image')}`);
+		}
+		o.addEventListener('change', () => {
+			previewLh0.setAttribute('src', `./assets/images/${o.getAttribute('preview-image')}`);
+		});
+	});
 	listLh1.forEach(o => {
 		if (o.checked) {
 			previewLh1.setAttribute('src', `./assets/images/${o.getAttribute('preview-image')}`);
@@ -33,6 +43,15 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 
 	setInterval(() => {
+		let cIdx0 = listLh0.findIndex(o => o.checked);
+		if (cIdx0 + 1 === listLh0.length) {
+			listLh0[0].checked = true;
+			previewLh0.setAttribute('src', `./assets/images/${listLh0[0].getAttribute('preview-image')}`);
+		} else {
+			listLh0[cIdx0 + 1].checked = true;
+			previewLh0.setAttribute('src', `./assets/images/${listLh0[cIdx0 + 1].getAttribute('preview-image')}`);
+		}
+		//
 		let cIdx1 = listLh1.findIndex(o => o.checked);
 		if (cIdx1 + 1 === listLh1.length) {
 			listLh1[0].checked = true;
